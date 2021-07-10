e69b84c3dda2f0c78c965c036b07a43233b851e3
Tokiosman80F <Tokiosman80F@users.noreply.github.com>
2021-07-11 00:01:55 +0600

    updated

---
+++ Guess_number/script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const secretNumber=Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent=secretNumber;
+
+let score=20;
+
+//check function 
+document.querySelector('.check').addEventListener
+('click',function()
+{
+    const guess=Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    
+    //if no number inputed
+    if(!guess) 
+    {
+        document.querySelector('.message').textContent="No Number";
+    }
+    //when player wins 
+    else if (guess===secretNumber)
+    {
+        document.querySelector('.message').textContent="Correct Number";
+
+        //body change because player wins
+        document.querySelector('body').style.backgroundColor='#60b347';
+        document.querySelector('.number').style.width='30rem';
+    }
+    //when guess is too high
+    else if (guess>secretNumber)
+    {   
+        if(score>1)
+        {
+        document.querySelector('.message').textContent="Too High";
+        score--;
+        document.querySelector('.score').textContent=score;
+        }
+        else
+        {
+            document.querySelector('.message').textContent="You Lose The GAME";
+            document.querySelector('.score').textContent=0;   
+        }
+    }
+
+    //when guess is too low
+    else if(guess<secretNumber)
+    {
+        if(score>1)
+        {
+        document.querySelector('.message').textContent="Too Low";
+        score--;
+        document.querySelector('.score').textContent=score;
+        }
+        else
+        {
+            document.querySelector('.message').textContent="You Lose The Game";
+            document.querySelector('.score').textContent=0;
+         }    
+    }
+
+
+
+})
